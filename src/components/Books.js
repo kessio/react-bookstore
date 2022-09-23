03ffe20d00188c39073a5eabddb235f6b4d25454
kessio <64524758+kessio@users.noreply.github.com>
2022-09-23 23:10:02 +0300

    inititialise project

---
+++ src/components/Books.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Books() {
+  return (
+
+    <div className="pt-5 container">
+    <h3>List of books</h3>
+    <button className="btn-dark btn-small rounded" type="button">Remove book</button>
+    <div className="d-flex pt-5">
+      <input type="text" placeholder="Add your book" />
+    </div>
+  </div>
+    
+  )
+}
---
+++ src/components/Categories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Categories() {
+  return (
+    <div>
+    <h3>Under construction</h3>
+  </div>
+  )
+}
