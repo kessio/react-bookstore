2edd7aadf43eba29aac057c1fad0bca763d874e0
kessio <64524758+kessio@users.noreply.github.com>
2022-10-08 19:33:59 +0300

    finish styling display books

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,21 +7,55 @@ const Book = ({ title, author, id }) => {
   const dispatch = useDispatch();
   return (
     <div className="Lesson-Panel">
-      <div>
-      <span>Action</span>
-      <span>Current Chapter</span>
+      <div className="headings">
+        <span className="actions">Action</span>
+        <span className="chapter">CURRENT CHAPTER</span>
       </div>
-      <h5>{title}</h5>
-      <p>{author}</p>
-      <button
-        type="button"
-        id={id}
-        onClick={(e) => {
-          dispatch(deleteBook(e.target.id));
-        }}
-      >
-        Remove
-      </button>
+      <div className="content-wrap">
+        <div className="col-1">
+          <h5 className="title">{title}</h5>
+          <p className="author">{author}</p>
+          <div className="buttons">
+            <button type="button" className="actions-btn line">Comments</button>
+            <button
+              className="actions-btn line"
+              type="button"
+              id={id}
+              onClick={(e) => {
+                dispatch(deleteBook(e.target.id));
+              }}
+            >
+              Remove
+            </button>
+            <button type="button" className="actions-btn">Edit</button>
+          </div>
+        </div>
+        <div className="single-chart">
+          <svg viewBox="0 0 36 36" className="circular-chart blue">
+            <path
+              className="circle-bg"
+              d="M18 2.0845
+          a 15.9155 15.9155 0 0 1 0 31.831
+          a 15.9155 15.9155 0 0 1 0 -31.831"
+            />
+            <path
+              className="circle"
+              strokeDasharray="60, 100"
+              d="M18 2.0845
+          a 15.9155 15.9155 0 0 1 0 31.831
+          a 15.9155 15.9155 0 0 1 0 -31.831"
+            />
+          </svg>
+          <div>
+            <text className="percentage">60%</text>
+            <text className="completed">Completed</text>
+          </div>
+        </div>
+        <div className="currentchapter">
+          <div className="thechapter">Chapter 17: A lesson</div>
+          <button type="button" className="updateprogress"><span className="progresstxt">Update Progress</span></button>
+        </div>
+      </div>
     </div>
   );
 };
