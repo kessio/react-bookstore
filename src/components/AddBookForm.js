40b57f117f651879c4949dcf89b80eb00a09ad4a
kessio <64524758+kessio@users.noreply.github.com>
2022-10-08 19:57:06 +0300

    style form

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -10,48 +10,56 @@ function AddBookForm({
 
   return (
     <div>
-      Add Book
-      <form className="form">
-        <input
-          type="text"
-          placeholder="Title"
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Author"
-          onChange={(e) => setAuthor(e.target.value)}
-        />
-        <select
-          required
-          id="categoryList"
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option>Select Category</option>
-          <option>Drama</option>
-          <option>Classical</option>
-          <option>Fiction</option>
-        </select>
-        <button
-          type="button"
-          onClick={() => {
-            if (title && author && category) {
-              dispatch(
-                postBook({
-                  id: Math.floor(Math.random() * 1000),
-                  title,
-                  author,
-                  category,
-                }),
-              );
-              document.querySelector('.form').reset();
-            }
-          }}
-        >
-          Add
-        </button>
-      </form>
+      <div className="form-line" />
+      <div className="addbook-form">
+        <div className="form-title">ADD NEW BOOK</div>
+        <form className="form">
+          <input
+            type="text"
+            className="formfield"
+            placeholder="Book Title"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <input
+            type="text"
+            className="formfield"
+            placeholder="Author"
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+          <select
+            required
+            id="categoryList"
+            className="formfield"
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option>Select Category</option>
+            <option>Action</option>
+            <option>Science Fiction</option>
+            <option>Economy</option>
+          </select>
+          <button
+            type="button"
+            className="submitbook"
+            onClick={() => {
+              if (title && author && category) {
+                dispatch(
+                  postBook({
+                    id: Math.floor(Math.random() * 1000),
+                    title,
+                    author,
+                    category,
+                  }),
+                );
+                document.querySelector('.form').reset();
+              }
+            }}
+          >
+            Add Book
+          </button>
+        </form>
+      </div>
     </div>
+
   );
 }
 
